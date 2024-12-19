6750b518353ac1a215e7bbf2ff95d9605792e877
Jean Pierre <jeanpierre.webdev@gmail.com>
2024-12-19 14:14:03 -0500

    Updates environment variable names

--- lib/crons.ts
+++ lib/crons.ts
@@ -2,12 +2,12 @@ import { runChangelog } from '../Changelog/changelog.ts'
 
 // Run at 23:00 UTC on monday every monday
 Deno.cron('vscode_weekly_changelog', '0 23 * * 2', async () => {
-  const agentId = Deno.env.get('STUDIO_AGENT_ID')
+  const agentId = Deno.env.get('VSCODE_AGENT_ID')
   const channelId = Deno.env.get('VSCODE_DISCORD_CHANNEL_ID')
   const platformName = Deno.env.get('VSCODE_PLATFORM_NAME')
 
   if (!agentId) {
-    console.log('Missing STUDIO_AGENT_ID environment variable')
+    console.log('Missing VSCODE_AGENT_ID environment variable')
     return
   }
 
